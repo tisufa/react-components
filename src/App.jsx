be13ae3dcdc8486fa784be63b715b2dd979b8ca1
Titus Sutio Fanpula <tisufamoney@gmail.com>
2025-05-17 23:49:22 +0700

    first: commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="w-screen min-h-screen flex items-center justify-center">
+      <h1 className="bg-slate-100 p-3 rounded-sm">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
